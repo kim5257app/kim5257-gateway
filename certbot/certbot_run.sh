45a649196b9746daa48ea32137dc435f7108cfce
Jaegu Kim <kim5257@naver.com>
2023-05-14 07:22:14 +0900

    잘못된 경로 수정

--- certbot/certbot_run.sh
+++ certbot/certbot_run.sh
@@ -1,2 +1,2 @@
 certbot certonly --webroot --webroot-path=/home/cnslink/nfs/kim5257-gateway-nginx/htdocs/letsencrypt -d $1 --cert-path /home/cnslink/nfs/kim5257-gateway-nginx/cert --config-dir . --work-dir . --logs-dir .
-cp -R /home/cnslink/workspace/certbot/archive/$1 /home/cnslink/nfs/kim5257-gateway-nginx/cert/
+cp -R /home/cnslink/workspace/kim5257-gateway/certbot/archive/$1 /home/cnslink/nfs/kim5257-gateway-nginx/cert/
