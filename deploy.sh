0570f6c2e6ebe50cc792b614d6616de66bbd1379
Jaegu Kim <kim5257@naver.com>
2023-07-30 02:10:41 +0900

    1. Docker 서비스 설치 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-sed -i "s|$USER|\$USER|g" -i docker-compose.yml.template > docker-compose.yml
+sed "s/\$USER/$USER/g" docker-compose.yml.template > docker-compose.yml
 docker stack deploy --with-registry-auth --compose-file docker-compose.yml kim5257_gateway
